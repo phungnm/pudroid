a41489176df0cbd687c9007d9801db29ff3238b6
Phụng Minh <1512414@student.hcmus.edu.vn>
2020-02-20 09:51:50 +0700

    d

--- main.go
+++ main.go
@@ -10,34 +10,34 @@ import (
 	"net/http"
 	//"github.com/gin-contrib/sessions"
 	//"github.com/gin-contrib/sessions/cookie"
-	"github.com/thoas/go-funk"
+	//"github.com/thoas/go-funk"
 	"strconv"
 	"github.com/gin-contrib/cors"
 )
-func AuthenticationRequired(auths ...string) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		session := sessions.Default(c)
-		user := session.Get("user")
+// func AuthenticationRequired(auths ...string) gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		session := sessions.Default(c)
+// 		user := session.Get("user")
 
-		if user == nil {
-			c.JSON(http.StatusUnauthorized, gin.H{"error": "User needs to be signed in to access this service"})
-			c.Abort()
-			return
-		}
-		if len(auths) != 0 {
-			authType := session.Get("authType")
-			if authType == nil || !funk.ContainsString(auths, authType.(string)) {
-				c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
-				c.Abort()
-				return
-			}
-		}
-		// add session verification here, like checking if the user and authType
-		// combination actually exists if necessary. Try adding caching this (redis)
-		// since this middleware might be called a lot
-		c.Next()
-	}
-}
+// 		if user == nil {
+// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "User needs to be signed in to access this service"})
+// 			c.Abort()
+// 			return
+// 		}
+// 		if len(auths) != 0 {
+// 			authType := session.Get("authType")
+// 			if authType == nil || !funk.ContainsString(auths, authType.(string)) {
+// 				c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
+// 				c.Abort()
+// 				return
+// 			}
+// 		}
+// 		// add session verification here, like checking if the user and authType
+// 		// combination actually exists if necessary. Try adding caching this (redis)
+// 		// since this middleware might be called a lot
+// 		c.Next()
+// 	}
+// }
 func setupRouter() *gin.Engine {
 	router := gin.Default()
 
