5ea0e39f059d092cb0254ea549b0cac978c1480d
Phụng Minh <1512414@student.hcmus.edu.vn>
2020-02-20 09:54:31 +0700

    update

--- main.go
+++ main.go
@@ -6,52 +6,51 @@ import (
 	"github.com/gin-gonic/gin"
 	"pudroid/controllers"
 	"pudroid/config"
-	//"github.com/foolin/gin-template"
+	"github.com/foolin/gin-template"
 	"net/http"
-	//"github.com/gin-contrib/sessions"
-	//"github.com/gin-contrib/sessions/cookie"
-	//"github.com/thoas/go-funk"
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-contrib/sessions/cookie"
+	"github.com/thoas/go-funk"
 	"strconv"
 	"github.com/gin-contrib/cors"
 )
-// func AuthenticationRequired(auths ...string) gin.HandlerFunc {
-// 	return func(c *gin.Context) {
-// 		session := sessions.Default(c)
-// 		user := session.Get("user")
+func AuthenticationRequired(auths ...string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		session := sessions.Default(c)
+		user := session.Get("user")
 
-// 		if user == nil {
-// 			c.JSON(http.StatusUnauthorized, gin.H{"error": "User needs to be signed in to access this service"})
-// 			c.Abort()
-// 			return
-// 		}
-// 		if len(auths) != 0 {
-// 			authType := session.Get("authType")
-// 			if authType == nil || !funk.ContainsString(auths, authType.(string)) {
-// 				c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
-// 				c.Abort()
-// 				return
-// 			}
-// 		}
-// 		// add session verification here, like checking if the user and authType
-// 		// combination actually exists if necessary. Try adding caching this (redis)
-// 		// since this middleware might be called a lot
-// 		c.Next()
-// 	}
-// }
+		if user == nil {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "User needs to be signed in to access this service"})
+			c.Abort()
+			return
+		}
+		if len(auths) != 0 {
+			authType := session.Get("authType")
+			if authType == nil || !funk.ContainsString(auths, authType.(string)) {
+				c.JSON(http.StatusForbidden, gin.H{"error": "invalid request, restricted endpoint"})
+				c.Abort()
+				return
+			}
+		}
+		// add session verification here, like checking if the user and authType
+		// combination actually exists if necessary. Try adding caching this (redis)
+		// since this middleware might be called a lot
+		c.Next()
+	}
+}
 func setupRouter() *gin.Engine {
 	router := gin.Default()
-
 	router.Static("/public", "./public")
 	router.Static("/assets", "./assets")
-	// store := cookie.NewStore([]byte("secret"))
-	// router.Use(sessions.Sessions("mysession", store))
-	// //new template engine
-	// router.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
-	// 	Root:      "views/",
-	// 	Extension: ".html",
-	// 	Master:    "layouts/master",
-	// 	DisableCache: true,
-	// })
+	store := cookie.NewStore([]byte("secret"))
+	router.Use(sessions.Sessions("mysession", store))
+	//new template engine
+	router.HTMLRender = gintemplate.New(gintemplate.TemplateConfig{
+		Root:      "views/",
+		Extension: ".html",
+		Master:    "layouts/master",
+		DisableCache: true,
+	})
 
 	//ROUTER
 	shortenURL_api := router.Group("/api/shortenUrl")
@@ -78,19 +77,11 @@ func setupRouter() *gin.Engine {
 		ctx.HTML(http.StatusOK, ctx.Param("page"), gin.H{
 					"title": "URL Shortener", })
 	})
-	
 	return router
 }
 
 func main() {
   	router := setupRouter()
-  	router.Use(cors.New(cors.Config{
-		AllowMethods:     []string{"GET", "POST", "OPTIONS", "PUT"},
-		AllowHeaders:     []string{"Origin", "Content-Length", "Content-Type", "User-Agent", "Referrer", "Host", "Token"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-		AllowAllOrigins:  true,
-		MaxAge:           86400,
-	}))
+  	router.Use(cors.Default())
 	router.Run(":"+strconv.Itoa(config.Config.Port)) // Ứng dụng chạy tại cổng 3000
 }
