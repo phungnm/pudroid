b03b72748498c43da515d95500a215d971d498fc
Phungnm <phungnm1997@gmail.com>
2020-11-01 19:38:24 +0700

    new

--- assets/js/queryTable.js
+++ assets/js/queryTable.js
@@ -1,24 +1,9 @@
+var ip_rows = [];
+var workbook;
 function readExcelFile(e) {
     var files = e.target.files, f = files[0];
 	reader = new FileReader();
-    reader.onload = function(e) {
-      var data = new Uint8Array(e.target.result);
-      var workbook = XLSX.read(data, {type: 'array'});
-      var result = [];
-      workbook.SheetNames.forEach(function(name) {
-          result.push(XLSX.utils.sheet_to_json(workbook.Sheets[name],{header: 1}));
-      });
-	  console.log(workbook,result);
-	  ip_rows = result[0];
-	  if(ip_rows)
-	  {
-		  console.log(ip_rows);
-	  }
-
-  
-      /* DO SOMETHING WITH workbook HERE */
-    };
-    reader.readAsArrayBuffer(f);
+    
 }
 document.getElementById('tool-checkIP-input-file').addEventListener('change', function (e){
 	if(e.target.files.length!=0){
@@ -57,20 +42,23 @@ jQuery('#file').change(function(){
             offset += chunk_size;
             var blob = file.slice(offset, offset + chunk_size);
 
-            reader.readAsText(blob);
+            reader.readAsBinaryString(blob);
 
           } else {
             progress.html("processing teh content...");
-
             var content = chunks.join("");
-
-            alert("content is ready!");
-            debugger;
+             workbook = XLSX.read(content, {
+                type: 'binary'
+              });
+         // works
+         console.log(workbook['Sheets']['Sheet1']);
           };
         }
       };
 
+
       var blob = file.slice(offset, offset + chunk_size);
-      reader.readAsText(blob);
+      reader.readAsBinaryString(blob);
     }
+    
   });
